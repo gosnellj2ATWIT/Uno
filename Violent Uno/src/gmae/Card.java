187d3a8787512bcf51516a466c624f905f09c2e4
gosnellj2 <gosnellj2@PF2YN8HK.wit.private>
2022-04-02 11:26:56 -0400

    Card class

--- Violent Uno/src/gmae/Card.java
+++ Violent Uno/src/gmae/Card.java
@@ -1,5 +1,34 @@
 package gmae;
 
 public class Card {
-
+	private final Color color;
+	private final Value value;
+	private boolean isflipped;
+	enum Color {
+		Red, Blue, Green, Yellow, Wild;
+		private static final Color[] colors = Color.values();
+		public static Color getColor(int i) {
+			return Color.colors[i];
+		}
+	}
+	enum Value {
+		Zero, One, Two, Tree, Four, Five, Six, Seven, Eight, Nine, DrawTwo, Skip, Reverse, Wild, WildDrawFour;
+		private static final Value[] values = Value.values();
+		public static Value getValue(int i) {
+			return Value.values[i];
+		}
+	}
+	public Card(final Color color, final Value value) {
+		this.color = color;
+		this.value = value;
+	}
+	public Color getColor() {
+		return this.color;
+	}
+	public Value getValue() {
+		return this.value;
+	}
+	public String toString() {
+		return color + "_" + value;
+	}
 }
